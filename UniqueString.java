e4e59fab886b16c05e9526acee8ab7b16737ea8c
Walaa <walaa.elsayed@bibalex.org>
2017-03-20 21:53:56 +0200

    Update UniqueString.java

--- UniqueString.java
+++ UniqueString.java
@@ -1,3 +1,6 @@
+// Implement an algorithm to determine if a string has all unique characters. What if you can not use additional data structures?
+// cracking the code interview
+
 import java.util.Scanner;
 
 
